370d4a664ce641f447446300b68df0abaa3ccfe8
한은정 <141193305+onesilver04@users.noreply.github.com>
2024-07-13 17:04:48 +0900

    part 2

--- detector/darknet.py
+++ detector/darknet.py
@@ -1,1 +1,157 @@
-yolo신경망 생성
+# 필요한 library import
+from __future__ import division
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.autograd import Variable
+import numpy as np
+
+# parse_cfg 함수 정의하기, 구성 파일의 경로를 입력으로 받습니다.
+def parse_cfg(cfgfile):
+    """
+    Takes a configuration file
+    
+    Returns a list of blocks. Each blocks describes a block in the neural
+    network to be built. Block is represented as a dictionary in the list
+    
+    """
+    
+    file = open(cfgfile, 'r')
+    lines = file.read().split('\n')                        # store the lines in a list
+    lines = [x for x in lines if len(x) > 0]               # get read of the empty lines 
+    lines = [x for x in lines if x[0] != '#']              # get rid of comments
+    lines = [x.rstrip().lstrip() for x in lines]           # get rid of fringe whitespaces
+    
+    block = {}
+    blocks = []
+    
+    for line in lines:
+        if line[0] == "[":               # This marks the start of a new block
+            if len(block) != 0:          # If block is not empty, implies it is storing values of previous block.
+                blocks.append(block)     # add it the blocks list
+                block = {}               # re-init the block
+            block["type"] = line[1:-1].rstrip()     
+        else:
+            key,value = line.split("=") 
+            block[key.rstrip()] = value.lstrip()
+    blocks.append(block)
+    
+    return blocks
+
+def create_modules(blocks):
+    net_info = blocks[0]     #Captures the information about the input and pre-processing    
+    module_list = nn.ModuleList()
+    prev_filters = 3
+    output_filters = []
+
+    for index, x in enumerate(blocks[1:]):
+        module = nn.Sequential()
+        
+        # block의 type을 확인합니다.
+        # block에 대한 새로운 module을 생성합니다.
+        # module_list에 append 합니다.
+
+        if (x["type"] == "convolutional"):
+            # layer에 대한 정보를 얻습니다.
+            activation = x["activation"]
+            try:
+                batch_normalize = int(x["batch_normalize"])
+                bias = False
+            except:
+                batch_normalize = 0
+                bias = True
+        
+            filters= int(x["filters"])
+            padding = int(x["pad"])
+            kernel_size = int(x["size"])
+            stride = int(x["stride"])
+        
+            if padding:
+                pad = (kernel_size - 1) // 2
+            else:
+                pad = 0
+        
+            # convolutional layer를 추가합니다.
+            conv = nn.Conv2d(prev_filters, filters, kernel_size, stride, pad, bias = bias)
+            module.add_module("conv_{0}".format(index), conv)
+        
+            # Batch Norm Layer를 추가합니다.
+            if batch_normalize:
+                bn = nn.BatchNorm2d(filters)
+                module.add_module("batch_norm_{0}".format(index), bn)
+        
+            # activation을 확인합니다.
+            # YOLO에서 Leaky ReLU 또는 Linear 입니다.
+            if activation == "leaky":
+                activn = nn.LeakyReLU(0.1, inplace = True)
+                module.add_module("leaky_{0}".format(index), activn)
+        
+            # upsampling layer 입니다.
+            # Bilinear2dUpsampling을 사용합니다.
+        elif (x["type"] == "upsample"):
+            stride = int(x["stride"])
+            upsample = nn.Upsample(scale_factor = 2, mode = "nearest")
+            module.add_module("upsample_{}".format(index), upsample)
+                
+        # route layer 입니다.
+        elif (x["type"] == "route"):
+            x["layers"] = x["layers"].split(',')
+            # route 시작
+            start = int(x["layers"][0])
+            # 1개만 존재하면 종료
+            try:
+                end = int(x["layers"][1])
+            except:
+                end = 0
+            # 양수인 경우
+            if start > 0: 
+                start = start - index
+            if end > 0:
+                end = end - index
+            route = EmptyLayer()
+            module.add_module("route_{0}".format(index), route)
+            # 음수 인 경우
+            if end < 0:
+                filters = output_filters[index + start] + output_filters[index + end]
+            else:
+                filters= output_filters[index + start]
+    
+        # skip connection에 해당하는 shortcut
+        elif x["type"] == "shortcut":
+            shortcut = EmptyLayer()
+            module.add_module("shortcut_{}".format(index), shortcut)
+            
+        # YOLO는 detection layer입니다.
+        elif x["type"] == "yolo":
+            mask = x["mask"].split(",")
+            mask = [int(x) for x in mask]
+    
+            anchors = x["anchors"].split(",")
+            anchors = [int(a) for a in anchors]
+            anchors = [(anchors[i], anchors[i+1]) for i in range(0, len(anchors),2)]
+            anchors = [anchors[i] for i in mask]
+    
+            detection = DetectionLayer(anchors)
+            module.add_module("Detection_{}".format(index), detection)
+        # loop의 마지막에 bookkeeping을 함.                      
+        module_list.append(module)
+        prev_filters = filters
+        output_filters.append(filters)
+    # loop의 전체 종료    
+    return (net_info, module_list)
+
+class DetectionLayer(nn.Module):
+    def __init__(self, anchors):
+        super(DetectionLayer, self).__init__()
+        self.anchors = anchors
+
+class EmptyLayer(nn.Module):
+    def __init__(self):
+        super(EmptyLayer, self).__init__()
+      
+# 잘 돌아가는 지 확인용
+blocks = parse_cfg("/content/drive/MyDrive/24summerproj.codeit/yolov3.cfg") 
+print(create_modules(blocks))
+
+    
