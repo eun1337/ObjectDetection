097a1e8fbc02665035c34293a3a7f1c7d159979f
한은정 <141193305+onesilver04@users.noreply.github.com>
2024-07-21 22:15:31 +0900

    VScode_part 5

--- detector/util.py
+++ detector/util.py
@@ -1,42 +1,69 @@
-# 다양한 helper functions 코드
 from __future__ import division
 
-import torch
+import torch 
 import torch.nn as nn
-import torch.nn.functional as F
+import torch.nn.functional as F 
 from torch.autograd import Variable
 import numpy as np
-import cv2
+import cv2 
 
 def unique(tensor):
     tensor_np = tensor.cpu().numpy()
     unique_np = np.unique(tensor_np)
     unique_tensor = torch.from_numpy(unique_np)
-
+    
     tensor_res = tensor.new(unique_tensor.shape)
     tensor_res.copy_(unique_tensor)
     return tensor_res
 
+
+def bbox_iou(box1, box2):
+    """
+    Returns the IoU of two bounding boxes 
+    
+    
+    """
+    #Get the coordinates of bounding boxes
+    b1_x1, b1_y1, b1_x2, b1_y2 = box1[:,0], box1[:,1], box1[:,2], box1[:,3]
+    b2_x1, b2_y1, b2_x2, b2_y2 = box2[:,0], box2[:,1], box2[:,2], box2[:,3]
+    
+    #get the corrdinates of the intersection rectangle
+    inter_rect_x1 =  torch.max(b1_x1, b2_x1)
+    inter_rect_y1 =  torch.max(b1_y1, b2_y1)
+    inter_rect_x2 =  torch.min(b1_x2, b2_x2)
+    inter_rect_y2 =  torch.min(b1_y2, b2_y2)
+    
+    #Intersection area
+    inter_area = torch.clamp(inter_rect_x2 - inter_rect_x1 + 1, min=0) * torch.clamp(inter_rect_y2 - inter_rect_y1 + 1, min=0)
+
+    #Union Area
+    b1_area = (b1_x2 - b1_x1 + 1)*(b1_y2 - b1_y1 + 1)
+    b2_area = (b2_x2 - b2_x1 + 1)*(b2_y2 - b2_y1 + 1)
+    
+    iou = inter_area / (b1_area + b2_area - inter_area)
+    
+    return iou
+
 def predict_transform(prediction, inp_dim, anchors, num_classes, CUDA = True):
 
-
+    
     batch_size = prediction.size(0)
     stride =  inp_dim // prediction.size(2)
     grid_size = inp_dim // stride
     bbox_attrs = 5 + num_classes
     num_anchors = len(anchors)
-
+    
     prediction = prediction.view(batch_size, bbox_attrs*num_anchors, grid_size*grid_size)
     prediction = prediction.transpose(1,2).contiguous()
     prediction = prediction.view(batch_size, grid_size*grid_size*num_anchors, bbox_attrs)
     anchors = [(a[0]/stride, a[1]/stride) for a in anchors]
 
-    # 중심 x,y 좌표와 object confidence를 SIgmoid 합니다.
+    #Sigmoid the  centre_X, centre_Y. and object confidencce
     prediction[:,:,0] = torch.sigmoid(prediction[:,:,0])
     prediction[:,:,1] = torch.sigmoid(prediction[:,:,1])
     prediction[:,:,4] = torch.sigmoid(prediction[:,:,4])
-
-    # 중심 offset을 추가합니다.
+    
+    #Add the center offsets
     grid = np.arange(grid_size)
     a,b = np.meshgrid(grid, grid)
 
@@ -51,7 +78,7 @@ def predict_transform(prediction, inp_dim, anchors, num_classes, CUDA = True):
 
     prediction[:,:,:2] += x_y_offset
 
-    # 높이와 넓이를 log space 변환합니다.
+    #log space transform height and the width
     anchors = torch.FloatTensor(anchors)
 
     if CUDA:
@@ -59,102 +86,92 @@ def predict_transform(prediction, inp_dim, anchors, num_classes, CUDA = True):
 
     anchors = anchors.repeat(grid_size*grid_size, 1).unsqueeze(0)
     prediction[:,:,2:4] = torch.exp(prediction[:,:,2:4])*anchors
-
-    # class score에 sigmoid activation을 적용합니다.
+    
     prediction[:,:,5: 5 + num_classes] = torch.sigmoid((prediction[:,:, 5 : 5 + num_classes]))
 
-    # detection map을 입력 이미지의 크기로 resize 합니다.
     prediction[:,:,:4] *= stride
-
+    
     return prediction
 
 def write_results(prediction, confidence, num_classes, nms_conf = 0.4):
-    # threshold 보다 낮은 objectness score를 갖은 bounding box의 속성을 0으로 설정
     conf_mask = (prediction[:,:,4] > confidence).float().unsqueeze(2)
     prediction = prediction*conf_mask
-
-    # bounding box의 중심 점을 좌측 상단, 우측 하단 모서리 좌표로 변환하기
+    
     box_corner = prediction.new(prediction.shape)
     box_corner[:,:,0] = (prediction[:,:,0] - prediction[:,:,2]/2)
     box_corner[:,:,1] = (prediction[:,:,1] - prediction[:,:,3]/2)
-    box_corner[:,:,2] = (prediction[:,:,0] + prediction[:,:,2]/2)
+    box_corner[:,:,2] = (prediction[:,:,0] + prediction[:,:,2]/2) 
     box_corner[:,:,3] = (prediction[:,:,1] + prediction[:,:,3]/2)
     prediction[:,:,:4] = box_corner[:,:,:4]
-
+    
     batch_size = prediction.size(0)
 
     write = False
+    
 
 
-    # 한번에 하나의 이미지에 대하여 수행
     for ind in range(batch_size):
         image_pred = prediction[ind]          #image Tensor
-       #confidence threshholding
+       #confidence threshholding 
        #NMS
-
-        # 가장 높은 값을 가진 class score를 제외하고 모두 삭제
+    
         max_conf, max_conf_score = torch.max(image_pred[:,5:5+ num_classes], 1)
         max_conf = max_conf.float().unsqueeze(1)
         max_conf_score = max_conf_score.float().unsqueeze(1)
         seq = (image_pred[:,:5], max_conf, max_conf_score)
         image_pred = torch.cat(seq, 1)
-
-        # threshold보다 낮은 object confidence를 지닌 bounding box rows를 0으로 설정한 것을 제거
+        
         non_zero_ind =  (torch.nonzero(image_pred[:,4]))
         try:
             image_pred_ = image_pred[non_zero_ind.squeeze(),:].view(-1,7)
         except:
             continue
-
-        # PyToch 0.4 호환성
-        # scalar가 PyTorch 0.4에서 지원되기 때문에 no detection에 대한
-        # not raise exception 코드입니다.
+        
         if image_pred_.shape[0] == 0:
-            continue
-#
-
-        # 이미지에서 검출된 다양한 classes를 얻기
-        img_classes = unique(image_pred_[:,-1])  # -1 index는 class index를 지니고 있습니다.
-
-
+            continue       
+#        
+  
+        #Get the various classes detected in the image
+        img_classes = unique(image_pred_[:,-1])  # -1 index holds the class index
+        
+        
         for cls in img_classes:
-            # NMS 실행하기
+            #perform NMS
 
-
-            # 특정 클래스에 대한 detections 얻기
+        
+            #get the detections with one particular class
             cls_mask = image_pred_*(image_pred_[:,-1] == cls).float().unsqueeze(1)
             class_mask_ind = torch.nonzero(cls_mask[:,-2]).squeeze()
             image_pred_class = image_pred_[class_mask_ind].view(-1,7)
-
-            # 가장 높은 objectness를 지닌 detections 순으로 정렬하기
-            # confidence는 가장 위에 있습니다.
+            
+            #sort the detections such that the entry with the maximum objectness
+            #confidence is at the top
             conf_sort_index = torch.sort(image_pred_class[:,4], descending = True )[1]
             image_pred_class = image_pred_class[conf_sort_index]
-            idx = image_pred_class.size(0)   #detections의 수
-
+            idx = image_pred_class.size(0)   #Number of detections
+            
             for i in range(idx):
-                # 모든 박스에 대해 하나하나 IOU 얻기
+                #Get the IOUs of all boxes that come after the one we are looking at 
+                #in the loop
                 try:
-                    #  i 인덱스를 갖고 있는 box의 IoU와 i보다 큰 인덱스를 지닌 bounding boxes 얻기
                     ious = bbox_iou(image_pred_class[i].unsqueeze(0), image_pred_class[i+1:])
                 except ValueError:
                     break
-
+            
                 except IndexError:
                     break
-
-                # IoU > threshhold인 detections를 0으로 만들기
+            
+                #Zero out all the detections that have IoU > treshhold
                 iou_mask = (ious < nms_conf).float().unsqueeze(1)
-                image_pred_class[i+1:] *= iou_mask
-
-                # non-zero 항목 제거하기
+                image_pred_class[i+1:] *= iou_mask       
+            
+                #Remove the non-zero entries
                 non_zero_ind = torch.nonzero(image_pred_class[:,4]).squeeze()
                 image_pred_class = image_pred_class[non_zero_ind].view(-1,7)
-
+                
             batch_ind = image_pred_class.new(image_pred_class.size(0), 1).fill_(ind)      #Repeat the batch_id for as many detections of the class cls in the image
-            # 이미지에 있는 class의 detections 만큼 batch_id를 반복합니다.
             seq = batch_ind, image_pred_class
-
+            
             if not write:
                 output = torch.cat(seq,1)
                 write = True
@@ -166,51 +183,33 @@ def write_results(prediction, confidence, num_classes, nms_conf = 0.4):
         return output
     except:
         return 0
-
-# iou 계산하기
-def bbox_iou(box1, box2):
-    # bounding boxes의 좌표를 얻습니다.
-    b1_x1, b1_y1, b1_x2, b1_y2 = box1[:,0], box1[:,1], box1[:,2], box1[:,3]
-    b2_x1, b2_y1, b2_x2, b2_y2 = box2[:,0], box2[:,1], box2[:,2], box2[:,3]
-
-    # intersection rectangle 좌표를 얻습니다.
-    inter_rect_x1 =  torch.max(b1_x1, b2_x1)
-    inter_rect_y1 =  torch.max(b1_y1, b2_y1)
-    inter_rect_x2 =  torch.min(b1_x2, b2_x2)
-    inter_rect_y2 =  torch.min(b1_y2, b2_y2)
-
-    #Intersection 영역
-    inter_area = torch.clamp(inter_rect_x2 - inter_rect_x1 + 1, min=0) * torch.clamp(inter_rect_y2 - inter_rect_y1 + 1, min=0)
-
-    # Union 영역
-    b1_area = (b1_x2 - b1_x1 + 1)*(b1_y2 - b1_y1 + 1)
-    b2_area = (b2_x2 - b2_x1 + 1)*(b2_y2 - b2_y1 + 1)
-
-    iou = inter_area / (b1_area + b2_area - inter_area)
-
-    return iou
-
+    
 def letterbox_image(img, inp_dim):
-    ''' padding을 사용하여 aspect ratio가 변화하지 않고 이미지를 resize 합니다.'''
+    '''resize image with unchanged aspect ratio using padding'''
     img_w, img_h = img.shape[1], img.shape[0]
     w, h = inp_dim
     new_w = int(img_w * min(w/img_w, h/img_h))
     new_h = int(img_h * min(w/img_w, h/img_h))
     resized_image = cv2.resize(img, (new_w,new_h), interpolation = cv2.INTER_CUBIC)
-
+    
     canvas = np.full((inp_dim[1], inp_dim[0], 3), 128)
 
     canvas[(h-new_h)//2:(h-new_h)//2 + new_h,(w-new_w)//2:(w-new_w)//2 + new_w,  :] = resized_image
-
+    
     return canvas
 
 def prep_image(img, inp_dim):
     """
-    신경망에 입력하기 위한 이미지 준비
-    변수를 반환합니다.
+    Prepare image for inputting to the neural network. 
+    
+    Returns a Variable 
     """
-
-    img = cv2.resize(img, (inp_dim, inp_dim))
+    img = (letterbox_image(img, (inp_dim, inp_dim)))
     img = img[:,:,::-1].transpose((2,0,1)).copy()
     img = torch.from_numpy(img).float().div(255.0).unsqueeze(0)
     return img
+
+def load_classes(namesfile):
+    fp = open(namesfile, "r")
+    names = fp.read().split("\n")[:-1]
+    return names
